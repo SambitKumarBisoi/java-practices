0acf7a6ca6ee5488c18f0bce3105d0a1864be647
SambitKumarBisoi <skbisoi619@gmail.com>
2024-07-23 06:19:25 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,32 @@
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		int Size_No=2;
+		//Replace with your perfect size(1,2,3,4,or 5)
+		switch(Size_No) 
+		{
+		case 1:
+			System.out.println("Extra Small ");
+			break;
+		case 2:
+			System.out.println("Small");
+			break;
+		case 3:
+			System.out.println("Medium");
+			break;
+		case 4:
+			System.out.println("Large");
+			break;
+		case 5:
+			System.out.println("Extra Large");
+			break;
+			default:
+				System.out.println("Invalid Size");
+			    
+		}
+		
+	}
+}
+	
---
+++ pp.java
@@ -0,0 +1,9 @@
+public class pp {
+    public static void m1(String[] args) {
+        /// code
+    }
+
+    public static void m2(String[] args) {
+        /// code
+    }
+}
